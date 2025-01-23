4d6891a33e4ad34742dde1f317bc222ee35c4bf7
fakhir hassan <fakhirhassan@fakhirs-MacBook-Pro.local>
2025-01-24 00:36:07 +0500

    anagram

--- duplicate.py
+++ duplicate.py
@@ -1,15 +1,30 @@
 #best approach to find duolicate in an array
 nums = [1,2,3,4,1]
-# hashset = set()
+hashset = set()
 # for num in nums:
 #     if num in hashset:
 #         print("Duplicate found: ", num)
 #     else:
 #         hashset.add(num)
 
-#brute force
-for i in range(len(nums)):
-    for j in range(i+1, len(nums)):
-        if nums[i] == nums[j]:
-            print("Duplicate found: ", nums[i])
-            break
+# #brute force
+# for i in range(len(nums)):
+#     for j in range(i+1, len(nums)):
+#         if nums[i] == nums[j]:
+#             print("Duplicate found: ", nums[i])
+#             break
+
+# print(len(list(filter(lambda x: x%2==0, nums))))
+# print(len(list(filter(lambda x: x%2!=0, nums))))
+# print(nums)
+
+def is_subset(lst1, lst2):
+    # Your code goes here
+    if lst1 in lst2:
+        return True
+    else:
+        print(False)
+        
+    pass
+print(is_subset([1, 2, 3],[1,2,3,4,5,6]))
+

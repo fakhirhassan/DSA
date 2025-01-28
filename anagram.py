2bf1177d183340adc47841040995dbd3e5f6189a
fakhir hassan <fakhirhassan@fakhirs-MacBook-Pro.local>
2025-01-29 02:07:07 +0500

    new

--- anagram.py
+++ anagram.py
@@ -1,15 +1,25 @@
 s= "anagram"
 t= "nagaram"
+# def isAnagram(s,t):
+#     if len(s)!=len(t):
+#         return False
+#     hashsetS = {} 
+#     hashsetT = {}
+#     for i in range(len(s)):
+#         hashsetS[s[i]] = hashsetS.get(s[i],0)+1
+#         hashsetT[t[i]] = hashsetT.get(t[i],0)+1
+#     for c in hashsetS:
+#         if c not in hashsetT or hashsetT[c]!=hashsetS[c]:
+#             return False
+#     return True
+# print(isAnagram(s,t))
 def isAnagram(s,t):
     if len(s)!=len(t):
         return False
-    hashsetS = {} 
-    hashsetT = {}
-    for i in range(len(s)):
-        hashsetS[s[i]] = hashsetS.get(s[i],0)+1
-        hashsetT[t[i]] = hashsetT.get(t[i],0)+1
-    for c in hashsetS:
-        if c not in hashsetT or hashsetT[c]!=hashsetS[c]:
+    sortedS = sorted(s)
+    sortedT = sorted(t)
+    for i in range(len(sortedS)):
+        if sortedS[i]!=sortedT[i]:
             return False
     return True
 print(isAnagram(s,t))
--- groupAnagram.py
+++ groupAnagram.py
@@ -1,20 +1,20 @@
-#brute force approach 
+#    brute force approach 
 from collections import defaultdict
-# str = ["eat", "tea", "tan", "ate", "nat", "bat"]
-# def groupAnagram(str):
-#     res = []
-#     for i in range(len(str)):
-#         if str[i] != "":
-#             temp = [str[i]]
-#             for j in range(i+1, len(str)):
-#                 if sorted(str[i]) == sorted(str[j]):
-#                     temp.append(str[j])
-#                     str[j] = ""
-#             res.append(temp)
-#     return res
-# print(groupAnagram(str))
+str = ["eat", "tea", "tan", "ate", "nat", "bat"]
+def groupAnagram(str):
+    res = []
+    for i in range(len(str)):
+        if str[i] != "":
+            temp = [str[i]]
+            for j in range(i+1, len(str)):
+                if sorted(str[i]) == sorted(str[j]):
+                    temp.append(str[j])
+                    str[j] = ""
+            res.append(temp)
+    return res
+print(groupAnagram(str))
 
-#optimal approach
+#   optimal approach
 
 str = ["eat", "tea", "tan", "ate", "nat", "bat"]
 def groupAnagram(str):
